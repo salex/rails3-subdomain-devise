c4820ec019a79139f42ce5918ac438c5277907c1
Steve Alex <salex@mac.com>
2010-08-19 21:22:55 -0500

    some installation instructions

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 puts 'SETTING UP EXAMPLE USERS'
-user1 = User.create! :name => 'First User', :subdomain_name => "foo", :email => 'user@test.com', :password => 'please', :password_confirmation => 'please'
+user1 = User.create! :name => 'Foo First User', :subdomain_name => "foo", :email => 'user@test.com', :password => 'please', :password_confirmation => 'please'
 puts 'New user created: ' << user1.name
 user2 = User.create! :name => 'Other User', :subdomain_name => "bar", :email => 'otheruser@test.com', :password => 'please', :password_confirmation => 'please'
 puts 'New user created: ' << user2.name
@@ -15,3 +15,5 @@ subdomain1 = Subdomain.create! :name => 'foo', :user_id => user1.id
 puts 'Created subdomain: ' << subdomain1.name
 subdomain2 = Subdomain.create! :name => 'bar', :user_id => user2.id
 puts 'Created subdomain: ' << subdomain2.name
+user3 = User.create! :name => 'Foo Second User', :subdomain_name => "foo", :email => 'user2@test.com', :password => 'please', :password_confirmation => 'please'
+user3 = User.create! :name => 'Foo Third User', :subdomain_name => "foo", :email => 'user3@test.com', :password => 'please', :password_confirmation => 'please'
